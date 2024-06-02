b96083dfa5dc0648ec08d7631456535ef4454507
Stellamaris Nwafor <wendumaris@gmail.com>
2024-06-02 04:20:51 +0100

    view awaiting approval

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -98,8 +98,10 @@ const Home = () => {
 				<div className="flex gap-2 justify-between items-center border-b border-[#D7D8DA] py-3 mt-10 ">
 					<div className="flex justify-start items-center gap-2">
 						<h2 className="font-semibold text-xl text-[#0B101A] ">신청 목록</h2>
-						<p className="font-medium text-sm text-[#5A616A]">
-							(총 100명 | 승인대기 <span className="text-[#FF4D4F] underline">50</span>건)
+						<p className="font-medium text-sm leading-4 text-[#5A616A]">
+							(총 {data.length}명 | 승인대기&nbsp;
+							<span className="text-[#FF4D4F] underline">{data.filter((item) => item.승인여부 === "승인대기").length}</span>
+							건)
 						</p>
 					</div>
 					<div className="flex flex-wrap justify-start items-center gap-1 ">
